b55c7a54f294803a7f38612b6b49208a124f43a6
MarceloMosl <marcelomosl@icloud.com>
2023-01-06 15:11:08 -0300

    requisito: GetTweets

--- src/app.js
+++ src/app.js
@@ -9,10 +9,23 @@ server.listen(5000, () => console.log("Ola"));
 
 const users = [];
 
+const tweets = [
+  {
+    username: "bobesponja",
+    tweet: "eu amo o hub",
+    avatar:
+      "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
+  },
+];
+
 server.post("/sign-up", (req, res) => {
   const user = req.body;
 
   users.push(user);
 
   res.send({ response: "ok" });
+});
+
+server.get("/tweets", (req, res) => {
+  res.send(tweets);
 });
